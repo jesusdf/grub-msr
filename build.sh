264124486de5c2b5a57793077ad7578699a2d894
Jesús Diéguez Fernández <jesusdf@gmail.com>
2019-02-24 02:50:26 +0100

    Add some requested changes to the code.
    Replace in all files s/__asm__ __volatile__/asm volatile/g.
    Include the module wrmsr in the forbidden modules efi list.

--- build.sh
+++ build.sh
@@ -2,13 +2,21 @@
 SCRIPT=$(realpath $0)
 ORIG=$(dirname $SCRIPT)
 cd /usr/src
-echo "Downloading grub source code..."
 #apt-get source grub-pc
-if [ ! -d /usr/src/grub ]; then
-    git clone git://git.savannah.gnu.org/grub.git
+if [ -d /usr/src/grub ]; then
+    rm -rf /usr/src/grub
 fi
+echo "Downloading grub source code..."
+git clone git://git.savannah.gnu.org/grub.git
 cd grub
-echo "Adding msr module..."
+echo "Patching __asm__ and __volatile__..."
+#find ./ -type f -name \*.[ch] -exec sed -i 's/__asm__(/__asm__ (/g;' {} \;
+#find ./ -type f -name \*.[ch] -exec sed -i 's/__asm__/    asm/g;s/__volatile__/    volatile/g;' {} \;
+#find ./ -type f -name \*.[ch] -exec sed -i 's/    asm     volatile/asm volatile/g;' {} \;
+find ./ -type f -name \*.[ch] -exec sed -i 's/__asm__ __volatile__/asm volatile/g;' {} \;
+echo "Including the wrmsr module in the EFI disabled module list..."
+sed -i "s/\"memrw\", NULL/\"memrw\", \"wrmsr\", NULL/g" /usr/src/grub/grub-core/commands/efi/shim_lock.c
+echo "Adding new rdmsr and wrmsr modules..."
 cp $ORIG/*.c ./grub-core/commands/i386/
 cp $ORIG/*.h ./include/grub/i386/
 if [ "$(cat ./grub-core/Makefile.core.def | grep msr | wc -l)" -eq "0" ]; then
